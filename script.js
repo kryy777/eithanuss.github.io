9c3636d1e49f0f8494eeed7e4badbf5f331380a4
kryy777 <164209846+kryy777@users.noreply.github.com>
2025-03-19 21:51:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Página de donación de médula cargada');
   
-  // BASTIAN AZOCAR - ULAGOS ICINF
-  document.addEventListener('mousemove', (e) => {
-    const skeleton = document.querySelector('.skeleton');
-    const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    
-    skeleton.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-  });
-
-  // BASTIAN AZOCAR - ULAGOS ICINF
-  if (window.matchMedia('(max-width: 768px)').matches) {
-    const skeleton = document.querySelector('.skeleton');
-    skeleton.style.transform = 'none';
+  
+  const skeleton = document.querySelector('.skeleton');
+  skeleton.style.transform = 'translateX(0) rotateY(0)';
+  
+  if (!window.matchMedia('(max-width: 768px)').matches) {
+    document.addEventListener('mousemove', (e) => {
+      const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+      const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+      
+      skeleton.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
   }
 });
+
+// BASTIAN AZOCAR ICINF ULAGOS
